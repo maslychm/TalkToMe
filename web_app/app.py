c9603b2b0a32164ebf09db4d926e6b83c56d52f6
Mykola Maslych <maslychm@gmail.com>
2021-11-14 03:49:45 -0500

    stuff

--- web_app/app.py
+++ web_app/app.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template, request
+from tensorflow.keras.models import Sequential
+from keras.layers.normalization import layer_normalization
+import tensorflow as tf
+# import tensorflow_hub as hub
+# import tensorflow_text as text
 
+import numpy as np
 import gpt_2_simple as gpt2
-# GPT2_SESSION = None
 
 app = Flask(__name__)
 app.static_folder = 'static'
-
-
-# def load_model():
-#     print(">>>>>>>Loading...")
-#     global GPT2_SESSION
-#     GPT2_SESSION = gpt2.start_tf_sess()
-#     gpt2.load_gpt2(GPT2_SESSION, run_name='things2\\run1')
-    
-#     print(">>>>>>>Modal was loaded")
 
 
 @app.route("/")
@@ -23,7 +19,7 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    # global GPT2_SESSION
+
     userText = request.args.get('msg')
     userText = str(userText)
     print(userText)
@@ -31,21 +27,29 @@ def get_bot_response():
     print(">>>>>>>Loading...")
     GPT2_SESSION = gpt2.start_tf_sess()
     gpt2.load_gpt2(GPT2_SESSION, run_name='things2\\run1')
-    
-    print(">>>>>>>Modal was loaded")
-    
+
+    print(">>>>>>>Model was loaded")
+
     results = gpt2.generate(GPT2_SESSION, length=75, temperature=1.0, prefix=f'Mykola: {userText} <|endoftext|> Steven: ',
                             nsamples=10, batch_size=5, return_as_list=True)
+
+    results = [res.split('<|endoftext|>')[1] for res in results]
+    print("All", results)
+    result = max(results, key=len)
     
-    ret = max(results, key=len)
-    print(ret, results)
+    # bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+    # bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
     
-    ret = results[2].split('<|endoftext|>')[1]
-    ret = ret.replace("Steven:", '')
-    return ret
+    # fpath = 'C:/Users/masly/projects/TalkToMe/web_app/checkpoint/cp.ckpt/'
+    # print(fpath)
+    # model = tf.keras.models.load_model(fpath)
+    # response = results[np.argmax(model.predict(results))]
+    # print("Chosen: ", response)
+    # ret = response.replace("Steven:", '')
+    
+    result = result.replace("Steven:", '')
+    return result
 
 
 if __name__ == "__main__":
-    # load_model()
     app.run()
-
