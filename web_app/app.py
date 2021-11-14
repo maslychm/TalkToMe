043fb747581127a3c035cfa45bfff2b9d90df908
Mostafa Aldilati <mdilaty@yahoo.com>
2021-11-13 22:58:54 -0500

    Added web app

---
+++ web_app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    
+    return "GET A LIFE"
+
+
+if __name__ == "__main__":
+    app.run()
